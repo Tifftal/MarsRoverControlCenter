0cf2b43c7cf2254991fadf7ba3ff16bc6590db66
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-10-28 15:26:10 +0300

    hotfix

---
+++ MarsRover/src/widgets/MarkersTab/MarkersTab.jsx
@@ -0,0 +1,18 @@
+export const MarkersTab = () => {
+
+    // Мне надо закрепить этот div в левом верхнем углу карты
+    return (
+        <div
+            style={{
+                position: "absolute",
+                top: "0px",
+                left: "0px",
+                width: "100px",
+                height: "100px",
+                backgroundColor: "red",
+            }}
+        >
+            <h1>MarkersTab</h1>
+        </div>
+    )
+}
