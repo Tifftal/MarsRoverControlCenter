8326bf4b583266d442759e318326d44ca6384aa6
VarTy <talankina.v@mail.ru>
2023-10-28 18:50:27 +0300

    about

--- MarsRover/src/App.jsx
+++ MarsRover/src/App.jsx
@@ -5,7 +5,6 @@ import MainPage from './pages/MainPage/MainPage';
 import { Map } from './pages/Map/Map';
 import Control from './pages/RoverControl/Control';
 import Navbar from './shared/Navbar/Navbar';
-import SideBar from './widgets/SideBar/SideBar';
 
 function App() {
   const [count, setCount] = useState(0)
--- MarsRover/src/pages/Map/Map.jsx
+++ MarsRover/src/pages/Map/Map.jsx
@@ -1,7 +1,6 @@
 import { SideTab } from "../../shared/SideTab/SideTab";
 import { Mapper } from "../../widgets/Map/Mapper";
 import { MapWrapper } from "../../widgets/MapWrapper/MapWrapper";
-import { MarkersTab } from "../../widgets/MarkersTab/MarkersTab";
 
 export const Map = () => {
     return (
