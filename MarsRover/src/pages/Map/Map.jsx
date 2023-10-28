a6b6eafe60b9b345f69f905c6dee4e06bd776a9f
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-10-28 15:20:30 +0300

    Map Position

--- MarsRover/src/pages/Map/Map.jsx
+++ MarsRover/src/pages/Map/Map.jsx
@@ -1,11 +1,25 @@
 import { Mapper } from "../../widgets/Map/Mapper";
+import { MapWrapper } from "../../widgets/MapWrapper/MapWrapper";
 import "./map.css";
 
 export const Map = () => {
     return (
-        
-            <Mapper />
-        
+        <>
+            <MapWrapper>
+            <div
+                style={{
+                    position: "absolute",
+                    left: "0",
+                    width: "200px",
+                    height: "200px",
+                    zIndex: "999"
+                }}
+            >
+                ПОПА
+            </div>
+                <Mapper />
+            </MapWrapper>
+        </>
     )
 };
 
