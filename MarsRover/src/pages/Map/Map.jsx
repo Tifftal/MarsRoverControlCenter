c3ed9e2fc73ef848ba0f0cac69f90dc329f35a92
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-10-28 15:21:17 +0300

    hotfix

--- MarsRover/src/pages/Map/Map.jsx
+++ MarsRover/src/pages/Map/Map.jsx
@@ -1,6 +1,5 @@
 import { Mapper } from "../../widgets/Map/Mapper";
 import { MapWrapper } from "../../widgets/MapWrapper/MapWrapper";
-import "./map.css";
 
 export const Map = () => {
     return (
@@ -10,6 +9,7 @@ export const Map = () => {
                 style={{
                     position: "absolute",
                     left: "0",
+                    top: "0",
                     width: "200px",
                     height: "200px",
                     zIndex: "999"
