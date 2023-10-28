d19b0867fd0ed0cfd57f5dbb6241eae9552d0ae8
VarTy <talankina.v@mail.ru>
2023-10-29 02:31:36 +0300

    Sample-cards

--- MarsRover/src/pages/MainPage/MainPage.jsx
+++ MarsRover/src/pages/MainPage/MainPage.jsx
@@ -4,12 +4,14 @@ import './MainPage.css'
 import Welcome from '../../widgets/Welcome/Welcome'
 import AboutPage from '../../widgets/AboutPage/AboutPage'
 import { Footer } from '../../widgets/Footer/Footer'
+import SampleCards from '../../widgets/SampleCards/SampleCards'
 
 const MainPage = () => {
     return (
         <div className='main'>
             <Welcome />
             <AboutPage />
+            <SampleCards />
             <Footer />
         </div>
     )
