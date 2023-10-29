e6a6d9aeebd03732c504279360fa26f5f66dbe69
VarTy <talankina.v@mail.ru>
2023-10-29 18:10:54 +0300

    ntfy

--- MarsRover/src/pages/Map/Map.jsx
+++ MarsRover/src/pages/Map/Map.jsx
@@ -73,31 +73,6 @@ export const Map = () => {
             })
     }
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const HandleOpenNote = () => {
-        setIsOpen(true)
-    };
-
-    const HandleCloseNote = () => {
-        setIsOpen(false)
-    };
-
-    const model = {
-        id: 5,
-        x: 1.77,
-        y: 1.77,
-        obstacleTraverseCoefficient: 1.77,
-        roubion: 1.77,
-        montdenier: 1.77,
-        montagnac: 1.77,
-        salette: 1.77,
-        robine: 1.77,
-        swiftRun: 1.77,
-        crosswindLake: 1.77,
-        roverId: 5,
-    };
-
     return (
         <>
             <MapWrapper>
@@ -127,50 +102,7 @@ export const Map = () => {
                         </Tabs>
                     ))}
                 </SideTab>
-                {isOpen && (
-                    <Popup onClose={HandleCloseNote} setIsOpen={setIsOpen}>
-                        <div>
-                            {/* Popup
-                            <p>{model.id}</p> */}
-                            <h4>Coordinates where the check took place</h4>
-                            <div style={{
-                                display: "flex",
-                                flexDirection: "row",
-                                alignItems: "center",
-                                gap: "10%",
-                            }}>
-                                <p>x: {model.x}</p>
-                                <p>y: {model.y}</p>
-                            </div>
-
-                            <h4 style={{
-                                marginTop: "2%",
-                            }}>
-                                ObstacleTraverseCoefficient</h4>
-                            <p>{model.obstacleTraverseCoefficient}</p>
-
-                            <h4 style={{
-                                marginTop: "2%",
-                            }}>
-                                Rocks discovered:</h4>
-                            <div style={{
-                                display: 'grid',
-                                gridTemplateColumns: 'repeat(2, 1fr)',
-                                gap: '2%',
-                            }}>
-                                <p>Roubion: {model.roubion}%</p>
-                                <p>Montdenier: {model.montdenier}%</p>
-                                <p>Montagnac: {model.montagnac}%</p>
-                                <p>Salette: {model.salette}%</p>
-                                <p>Robine: {model.robine}%</p>
-                                <p>SwiftRun: {model.swiftRun}%</p>
-                                <p>CrosswindLake: {model.crosswindLake}%</p>
-                                {/* <p>{model.roverId}</p> */}
-                            </div>
-                        </div>
-                    </Popup>
-                )}
-                <button onClick={HandleOpenNote}>Popup</button>
+                
                 {open && (
                     <Modal
                         header={point.name}
--- MarsRover/src/widgets/Notification/Notification.jsx
+++ MarsRover/src/widgets/Notification/Notification.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Notification.css";
+import { Popup } from "../../shared/PopupMission/Popup";
 
 function Notification({ message, onDelete }) {
     const [isClosing, setIsClosing] = useState(false);
@@ -21,10 +22,82 @@ function Notification({ message, onDelete }) {
         }
     }, [isClosing]);
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const HandleOpenNote = () => {
+        setIsOpen(true)
+    };
+
+    const HandleCloseNote = () => {
+        setIsOpen(false)
+    };
+
+    const model = {
+        id: 5,
+        x: 1.77,
+        y: 1.77,
+        obstacleTraverseCoefficient: 1.77,
+        roubion: 1.77,
+        montdenier: 1.77,
+        montagnac: 1.77,
+        salette: 1.77,
+        robine: 1.77,
+        swiftRun: 1.77,
+        crosswindLake: 1.77,
+        roverId: 5,
+    };
+
+
     return (
+
         <div className={`notification ${isClosing ? "closing" : "opening"}`}>
-            <button className="textNtfy">{message}</button>
-            <button className="closeBtn" onClick={closeNotification}>close</button>
+            {isOpen && (
+                <Popup onClose={HandleCloseNote} setIsOpen={setIsOpen}>
+                    <div>
+                        {/* Popup
+                            <p>{model.id}</p> */}
+                        <h4>Coordinates where the check took place</h4>
+                        <div style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            gap: "10%",
+                        }}>
+                            <p>x: {model.x}</p>
+                            <p>y: {model.y}</p>
+                        </div>
+
+                        <h4 style={{
+                            marginTop: "2%",
+                        }}>
+                            ObstacleTraverseCoefficient</h4>
+                        <p>{model.obstacleTraverseCoefficient}</p>
+
+                        <h4 style={{
+                            marginTop: "2%",
+                        }}>
+                            Rocks discovered:</h4>
+                        <div style={{
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(2, 1fr)',
+                            gap: '2%',
+                        }}>
+                            <p>Roubion: {model.roubion}%</p>
+                            <p>Montdenier: {model.montdenier}%</p>
+                            <p>Montagnac: {model.montagnac}%</p>
+                            <p>Salette: {model.salette}%</p>
+                            <p>Robine: {model.robine}%</p>
+                            <p>SwiftRun: {model.swiftRun}%</p>
+                            <p>CrosswindLake: {model.crosswindLake}%</p>
+                            {/* <p>{model.roverId}</p> */}
+                        </div>
+                    </div>
+                </Popup>
+            )}
+            <div className="visible-part">
+                <button className="textNtfy" onClick={HandleOpenNote}>{message}</button>
+                <button className="closeBtn" onClick={closeNotification}>close</button>
+            </div>
         </div>
     );
 }
